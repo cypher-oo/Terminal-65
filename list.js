d8e555633fbc0cc3bdd251bdffc06cc07188f87e
Yasir Ahmad <yasirahmad@Yasirs-MacBook-Air-2.local>
2020-12-22 02:17:06 +0530

    fixed

--- list.js
+++ list.js
@@ -1,4 +1,60 @@
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAQBgM9AFU9QB1F0h8O8aY2J0xHEoc6jV8",
+//     authDomain: "terminal65-8ac1a.firebaseapp.com",
+//     databaseURL: "https://terminal65-8ac1a-default-rtdb.firebaseio.com",
+//     projectId: "terminal65-8ac1a",
+//     storageBucket: "terminal65-8ac1a.appspot.com",
+//     messagingSenderId: "82005650969",
+//     appId: "1:82005650969:web:da04d479397228ea38c488"
+//   };
+//     // Initialize Firebase
+//     firebase.initializeApp(firebaseConfig);
+
+//   var userDataRef = firebase.database().ref("vendors").orderByKey();
+//   userDataRef.once("value").then(function(snapshot) {
+//   snapshot.forEach(function(childSnapshot) {
+//     var key = childSnapshot.key;
+//     var childData = childSnapshot.val();              
+//     var name_val = childSnapshot.val().vendor_name;
+//     var price = childSnapshot.val().price;
+//     var state = childSnapshot.val().state;
+//     var city = childSnapshot.val().city;
+//     var image=childSnapshot.val().image;
+    
+//     document.getElementById('test').innerHTML=childData['vendor_name']
+//     // document.getElementById('pic').src=childData['picture']
+//     document.getElementById('description').innerHTML=childData['description']
+//     document.getElementById('price').innerHTML='Rs '+ childData['price']
+//     document.getElementById('city').innerHTML=childData['city']+','+childData['state']
+//     document.getElementById('capacity').innerHTML=childData['capacity']+"+"
+//     document.getElementById('address').innerHTML=childData['address']
+//     document.getElementById('capacitys').innerHTML=childData['capacity']
+//     document.getElementById('ideal').innerHTML=childData['ideal']
+//     document.getElementById('ac').innerHTML=childData['ac']
+//     document.getElementById('wifi').innerHTML=childData['wifi']
+//     document.getElementById('pool').innerHTML=childData['pool']
+//     document.getElementById('drinking').innerHTML=childData['drinking']
+//     document.getElementById('outside').innerHTML=childData['outside_food']
+//     document.getElementById('music').innerHTML=childData['music']
+//     document.getElementById('backup').innerHTML=childData['backup']
+//     console.log(childData)
+  
+ 
+//     });
+//   });
+
+  
+//listing code
+
+var vendor_name;
+
+var url = location.href; 
+vendor_name = url.split('?')[1].split('=')[1]
+console.log(vendor_name)
+console.log(url)
+var x = decodeURIComponent(vendor_name).toLocaleLowerCase()
+console.log(x)
 const firebaseConfig = {
     apiKey: "AIzaSyAQBgM9AFU9QB1F0h8O8aY2J0xHEoc6jV8",
     authDomain: "terminal65-8ac1a.firebaseapp.com",
@@ -10,48 +66,22 @@ const firebaseConfig = {
   };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-// const dr =firebase.database().ref("/vendors");
-// dr.on('value',(snapshot)=>{
-//   var drs =snapshot.val()
-//   if(drs!=null)
-//   {
-//   var keys =Object.keys(drs);
-//   let newService =[];
-//   for(var dr = 0 ;dr<keys.length;dr++){
-//     var k=keys[dr];
-//     newService.push({
-//       description:drs[k].description,
-//       ideal:drs[k].ideal,
-//       price:drs[k].price,
-//       image_url:drs[k].image,
-//       vendor_name:drs[k].vendor_name,
-//       tnc:drs[k].terms_and_conditions,
-//       city:drs[k].city,
-//       state:drs[k].state,
-      
-//       id:k,
-//     });
-//     document.getElementById('test').innerHTML=drs[k].vendor_name
-//     // document.getElementById('price').innerHTML='Rs '+ drs[k].price,
-//     // document.getElementById('description').innerHTML=drs[k].description,
-//     // document.getElementById('address').innerHTML=drs[k].city+","+drs[k].state,
-//     // document.getElementById('pic').src=drs[k].image
-//     console.log(newService)
-//   }
-  var userDataRef = firebase.database().ref("vendors").orderByKey();
+
+  var userDataRef = firebase.database().ref("/vendors/"+x);
+  
   userDataRef.once("value").then(function(snapshot) {
   snapshot.forEach(function(childSnapshot) {
+    console.log(snapshot.val().price)
     var key = childSnapshot.key;
-    var childData = childSnapshot.val();              
+    var childData = snapshot.val();              
     var name_val = childSnapshot.val().vendor_name;
     var price = childSnapshot.val().price;
     var state = childSnapshot.val().state;
     var city = childSnapshot.val().city;
     var image=childSnapshot.val().image;
-    
     document.getElementById('test').innerHTML=childData['vendor_name']
     // document.getElementById('pic').src=childData['picture']
-    document.getElementById('description').innerHTML=childData['description']
+    document.getElementById('description').innerHTML=
     document.getElementById('price').innerHTML='Rs '+ childData['price']
     document.getElementById('city').innerHTML=childData['city']+','+childData['state']
     document.getElementById('capacity').innerHTML=childData['capacity']+"+"
@@ -65,13 +95,17 @@ const firebaseConfig = {
     document.getElementById('outside').innerHTML=childData['outside_food']
     document.getElementById('music').innerHTML=childData['music']
     document.getElementById('backup').innerHTML=childData['backup']
-    console.log(childData)
-  
+   
  
     });
   });
 
   
+
+    
+
+
+
 
     
 
