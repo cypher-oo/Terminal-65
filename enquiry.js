b3d56c55506636fa7d1d4234b553ef2449092067
Ahmad Yasir <51242252+yasirahmad62@users.noreply.github.com>
2020-12-22 02:24:50 +0530

    Update enquiry.js

--- enquiry.js
+++ enquiry.js
@@ -223,7 +223,7 @@ vendors.forEach(elem => {
                                     </span>
                                 <span class="rating-count vendor-text">(20)</span></div>
                         </div>
-												<p><a href="/Users/yasirahmad/Downloads/Terminal-65-main/listing.html?name=${elem.vendor_name}">View Details</p>
+												<p><a href="/listing.html?name=${elem.vendor_name}">View Details</p>
                         <!-- /.Vendor Content -->
                     </div>
                     <!-- /.Vendor thumbnail -->
